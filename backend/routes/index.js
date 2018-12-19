26d3ad7bb032301c7f07e54b35031c55b9693d6c
ameerkabir <ameerkabir2@gmail.com>
2018-12-19 17:54:41 +0000

    added backend poiler plate

---
+++ backend/routes/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+// const {catchErrors}  = require("../handlers/errorHandler");
+// const numberToRoman = require("../controllers/numberToRoman");
+const homepage = require('../controllers/index')
+// const romanToNumber = require('../controllers/romanToNumber')
+const api = () => {
+  router.get('/', homepage);
+//   router.post("/api/roman", numberToRoman);
+//    router.post("/api/number", romanToNumber);
+
+  return router;
+};
+module.exports = api;
