ce65da2216f7fd9fea55d92ad03e9bb6747055de
ameerkabir <ameerkabir2@gmail.com>
2019-04-28 22:07:45 +0100

    Fixed errors in the controller

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -5,16 +5,16 @@ export const homepage = (req, res) => {
 };
 
 export const resultData = async (req, res) => {
-  console.log(req.body);
+
   const { startYourOwnBusiness } = await req.body;
   try {
-    if (startYourOwnBusiness) {
+    if (startYourOwnBusiness === 'yes') {
       const response = await getEntrepreneurship(opportunities);
       return res.status(200).json({
         data: response
       });
     }
-    return res.json(400).json({
+    return res.status(400).json({
       message:
         'We could not find any  data for this search the provide information'
     });
