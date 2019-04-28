58fc80912b59ae84143b295afc030eb1f51ea17b
ameerkabir <ameerkabir2@gmail.com>
2019-04-28 22:11:22 +0100

    Deleted the harcoded reults and now getting the data from the api and display the theme for now

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -7,54 +7,35 @@ const Wrapper = styled.ol`
 `;
 const FlexContainer = styled.div`
   /* display: grid; */
+  display: flex;
   justify-content: center;
   align-content: center;
 `;
-// @Todo after implementing the node bit you can use the below function to display the actual results from the backend
-const RenderValues = ({ values }) => {
-  return values && values[1].fullName;
-};
-export default class Home extends Component {
+const ListItem = styled.li`
+  display: flex;
+  margin: 0 auto;
+  background: cyan;
+  flex-shrink: 1;
+`;
+
+export default class Results extends Component {
   render() {
-    console.log(this.props.values);
+    const { data } = this.props.results;
+    console.log("props in result component", data);
     return (
       <Wrapper>
-        <h4>Here is the result</h4>
-        <br />
-        <p>
-          <strong>
-            applicant can find a job locally if he/she enhances his/her language
-            skills
-          </strong>
-        </p>
-        <br />
-
-        <li>
-          we suggest a integration related Training opportunity provided by PMI
-          Institute. Here is an{" "}
-          <a
-            href="mailto:academic-outreach@pmi-netherlands-chapter.org"
-            target="_blank"
-          >
-            email
-          </a>
-        </li>
-        <br />
-
-        <li>
-          {" "}
-          we suggest a integration related Training opportunity provided by
-          Refugee Start Force you can now apply here:{" "}
-          <a href="https://refugeestartforce.eu" target="blank">
-            refugeestartforce.eu
-          </a>
-        </li>
-        <br />
-
-        <p>
-          if you have questions or need a mentor to guide you through the
-          application process, contact us at contact@rafiqi.net
-        </p>
+        {!data ? (
+          <p>....Loading</p>
+        ) : (
+          data && (
+            <FlexContainer>
+              <ul>
+                {" "}
+                <ListItem>{data.data.map(item => item.Theme)}</ListItem>
+              </ul>
+            </FlexContainer>
+          )
+        )}
       </Wrapper>
     );
   }
