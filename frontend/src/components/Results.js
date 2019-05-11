ea379a0efe98c824a7d95f1995ea8ba10ae06ccf
ameerkabir <ameerkabir2@gmail.com>
2019-05-12 00:28:38 +0100

    added the correct result to the ui

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -1,36 +1,59 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-const Wrapper = styled.ol`
-  display: grid;
+const Wrapper = styled.div`
+  /* display: grid;
   grid-auto-columns: repeat(2, 2fr);
-  grid-auto-columns: 1fr;
+  grid-auto-columns: 1fr; */
 `;
 const FlexContainer = styled.div`
   /* display: grid; */
-  display: flex;
+  /* display: flex;
   justify-content: center;
-  align-content: center;
+  align-content: center; */
 `;
 const ListItem = styled.li`
-  display: flex;
-  margin: 0 auto;
-  background: cyan;
-  flex-shrink: 1;
+  /* display: flex; */
+  /* margin: 0 auto; */
+  /* background: cyan; */
+  /* flex-shrink: 1; */
 `;
 
 export default class Results extends Component {
   render() {
     const { data } = this.props.results;
+    console.log(data);
     return (
       <Wrapper>
         {!data ? (
           <p>....Loading</p>
+        ) : !Array.isArray(data.data) ? (
+          <p>{data.message}</p>
         ) : (
           data && (
             <FlexContainer>
+              <div>
+                Based on our lookup, we suggest the following opportunities:
+              </div>
+
               <ul>
-                {" "}
-                <ListItem>{data.data.map(item => item.Theme)}</ListItem>
+                {data.data.map(item => {
+                  return (
+                    <ListItem>
+                      {`A ${item.category} in ${
+                        item.city === "any" ? "globally" : item.city
+                      } provided by ${
+                        item.opportunity_name
+                      } and delivered in  ${item.mode_of_delivery} mode. `}
+                      <a
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href={item.info}
+                      >
+                        here
+                      </a>
+                    </ListItem>
+                  );
+                })}
               </ul>
             </FlexContainer>
           )
