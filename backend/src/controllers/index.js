89ac7346eeb1bea85518d81affcbb20dd93d5cae
ameerkabir <ameerkabir2@gmail.com>
2019-05-12 00:26:18 +0100

    removed depulicage functions

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -23,14 +23,14 @@ export const resultData = async (req, res) => {
   try {
     const opportunities = await RafiqiContext.findAll();
     const response = await fetchData(req, opportunities);
-    if(response.length) {
+    if (response.length) {
       return res.status(200).json({
         data: response
       });
     } else {
-      return res.status(400).json({
+      return res.status(201).json({
         message:
-            'We could not find any data for this search to provide any information'
+          'No opportunity suitable to your profile was found. We will contact in the future if a suitable opportunity comes up'
       });
     }
   } catch (e) {
@@ -73,53 +73,6 @@ export const getOpportunity = async (req, res) => {
 };
 
 export const countUniq = async (req, res) => {
-  console.log('is it here');
-  try {
-    const lengthOfUniqOpportunities = uniq(opportunities);
-    return res.status(200).json({
-      lengthOfUniqOpportunities: lengthOfUniqOpportunities.length
-    });
-  } catch (e) {
-    return res.status(500).json({ e });
-  }
-};
-
-export const CreateOpportunities = async (req, res) => {
-  try {
-    const opportunity = await RafiqiContext.create(req.body);
-    return res.status(200).json({
-      opportunity
-    });
-  } catch (e) {
-    return res.status(500).json({ e });
-  }
-};
-
-export const EditOpportunities = async (req, res) => {
-  try {
-    const { _id, data } = req.body;
-    const opportunity = await RafiqiContext.findOneAndUpdate({ _id }, data);
-    return res.status(200).json({
-      opportunity
-    });
-  } catch (e) {
-    return res.status(500).json({ e });
-  }
-};
-export const getOpportunity = async (req, res) => {
-  try {
-    const _id = req.params.id;
-    const opportunity = await RafiqiContext.findBy({ _id });
-    return res.status(200).json({
-      opportunity
-    });
-  } catch (e) {
-    return res.status(500).json({ e });
-  }
-};
-
-export const countUniq = async (req, res) => {
-  console.log('is it here');
   try {
     const lengthOfUniqOpportunities = uniq(opportunities);
     return res.status(200).json({
